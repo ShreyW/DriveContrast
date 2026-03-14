e13120c925579990eb389088634f3e1cf5a94706
Tushar Tyagi <tushartyagi1811@gmail.com>
2026-03-13 22:35:23 -0400

    Update Waymo subset script and README to use 3 training shards

---
+++ download_waymo_subset.py
@@ -0,0 +1,25 @@
+from huggingface_hub import hf_hub_download
+import os
+
+repo_id = "aaaaaap/unstructed"
+save_dir = "./data/waymo_subset"
+os.makedirs(save_dir, exist_ok=True)
+
+# Select 2 training shards and 1 validation shard. 
+# This provides a mix of examples while keeping the total footprint under ~50-100GB
+shards_to_download = [
+    "waymo/waymo_train_shard_0000.tar",
+    "waymo/waymo_train_shard_0001.tar",
+    "waymo/waymo_train_shard_0002.tar",
+    "waymo/waymo_val_shard_0000.tar"
+]
+
+for shard in shards_to_download:
+    print(f"Downloading {shard}...")
+    hf_hub_download(
+        repo_id=repo_id,
+        repo_type="dataset",
+        filename=shard,
+        local_dir=save_dir
+    )
+print("Finished downloading Waymo subset.")
